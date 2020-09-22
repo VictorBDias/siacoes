6bbecf570bcdaa52f0c4cb8670e4de50d4448a1f
Victor BDias <victor.batisttete@gmail.com>
2020-09-22 11:53:34 -0300

    up to date

---
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
@@ -0,0 +1,19 @@
+﻿package br.edu.utfpr.dv.siacoes.model;
+
+import lombok.Data;
+import java.io.Serializable;
+
+public @Data class Department implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int idDepartment = 0;
+	private Campus campus = new Campus();
+	private String name = "";
+	private String fullName = "";
+	private transient byte[] logo = null;
+	private boolean active = true;
+	private String site = "";
+	private String initials = "";
+	
+}
